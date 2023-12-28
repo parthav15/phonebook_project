c2a8a82c76544f5706dad42272d73ad05c4a2e92
parthav15 <parthavsabrwal@gmail.com>
2023-12-28 16:45:00 +0530

    minor change

--- phonebook_project/settings.py
+++ phonebook_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-3zrszhw**f_&r^mb+_y8plbm#n=11e_oqyqfmdidxhh2_by25m
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
